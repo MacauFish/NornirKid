46129598707446174636eb164e889a3ab3c3637b
Macau Fish <badboychcj@gmail.com>
2020-03-12 15:16:21 +0800

    First Commit

---
+++ test1.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+result = nr.run(
+    task = netmiko_send_command, command_string = 'show port 1 information'
+)
+
+print_result(result)
